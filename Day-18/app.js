155eb64d6eae842eea250d7b67f0715da1fe0c4a
Shubham Mishra <131137726+shubhammishra9039@users.noreply.github.com>
2024-08-27 23:24:45 +0530

    Add files via upload

---
+++ Day-18/app.js
@@ -0,0 +1,17 @@
+let eye = document.getElementById("eyes")
+let input = document.getElementById("input")
+console.log(input);
+
+function show()
+{
+    if(input.type="password")
+    {
+        input.type="text"
+        eye.src=
+    }
+    else
+    {
+        input.type="password"
+        
+    }
+}
